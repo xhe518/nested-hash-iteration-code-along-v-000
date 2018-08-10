ad0217ecd800238bc1dde03f8974c7beeef05ab4
Sid He <xhe518@gmail.com>
2018-08-10 03:48:53 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,6 +16,10 @@ require 'pry'
 
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    binding.pry
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+      end
+    end
   end
 end
